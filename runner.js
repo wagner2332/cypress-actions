d413240af5088d626c680c80dea97d991106a447
Wagner Wilson <55373928+wagner2332@users.noreply.github.com>
2024-05-28 22:07:19 -0300

    Update variable token

--- runner.js
+++ runner.js
@@ -1,14 +1,12 @@
 const cypress = require('cypress')
 const tesults = require('cypress-tesults-reporter');
 
-const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjY1YmZmOGNhLTQ5YzMtNDRmYi04ZWE2LTQxY2E1MmQxYzUzYS0xNzE2OTQzMDE0NzYyIiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiYWNjOWZkODItOTk1ZC00NGY2LWE3NzUtMTU0OTE2MTQzZmNmIiwidHlwZSI6InQifQ.303fg0j3fdWINvvDeoONCUsfHSVAsfcvydvXf9rqn9g'
-
 cypress.run({
   // specs to run here
 })
 .then((results) => {
   const args = {
-    target: token,
+    target: process.env.TARGET_TOKEN_ELECTRON,
   }
   tesults.results(results, args);
 })
